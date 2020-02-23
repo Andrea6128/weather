65ccf8c8a6649e5ac9109feb4c487e2e00126d11
Andrea <iam.andreapetriku@gmail.com>
2020-02-23 13:05:28 +0100

    first working version, still with no css

--- weather/models.py
+++ weather/models.py
@@ -20,29 +20,9 @@ class City(models.Model):
     # just one model in db
     cityName = models.CharField(max_length=30, verbose_name=('City Name'), choices=CITIES)
 
-    api_address = "http://api.openweathermap.org/data/2.5/weather?q=Prague&appid=fb7aed4ba4c5be80800a55852257ca28"
-
-    json_data = requests.get(api_address).json()
-    kelvin = json_data["main"]["temp"]
-    celsius = round(kelvin - 273.15, 2)
-
-    for item in json_data['weather']:
-        get_weather_type = item['main']
-
-    for item in json_data['weather']:
-        get_weather_desc = item['description']
-
-    currentTemp = celsius
-    weatherType = get_weather_type
-    weatherDesc = get_weather_desc
-    windSpeed = json_data["wind"]["speed"]
-    currentTime = datetime.now()
-    # time only updates when starting server - fix that
-
     # get city url by id for use in templates
     def getCityUrl(self):
         return reverse('city_detail_view', kwargs={'id': self.id})
-
 
     def __str__(self):
         return self.cityName
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-# from django.db.models import Q
 from .models import City
+from datetime import datetime
+import requests
 
 # Create your views here.
 
@@ -10,8 +11,32 @@ def home_view(request):
     context = { 'cities': queryset }
     return render(request, 'home_view.html', context)
 
-# city view by id for single city view
+# single city view by id
 def city_id_view(request, id):
     city = get_object_or_404(City, id=id)
-    context = { 'thiscity': city }
+
+    api_address = "http://api.openweathermap.org/data/2.5/weather?q=" + str(city) + "&appid=fb7aed4ba4c5be80800a55852257ca28"
+    print(api_address)
+    json_data = requests.get(api_address).json()
+    kelvin = json_data["main"]["temp"]
+    celsius = round(kelvin - 273.15, 2)
+
+    for item in json_data['weather']:
+        get_weather_type = item['main']
+
+    for item in json_data['weather']:
+        get_weather_desc = item['description']
+
+    currentTemp = celsius
+    weatherType = get_weather_type
+    weatherDesc = get_weather_desc
+    windSpeed = json_data["wind"]["speed"]
+    currentTime = datetime.now()
+
+    context = { 'thisCity': city,
+                'thisTemp': currentTemp,
+                'thisWeatherType': weatherType,
+                'thisWeatherDesc': weatherDesc,
+                'thisWindSpeed': windSpeed,
+                'thisCurrentTime': currentTime }
     return render(request, 'city.html', context)
